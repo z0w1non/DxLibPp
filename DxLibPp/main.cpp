b8faa773244bff6a38431fa0bfeaae9059edc58b
Nozomi <40842954+z0w1non@users.noreply.github.com>
2019-01-02 00:46:28 +0900

    Change over to Vidual Studio

---
+++ DxLibPp/main.cpp
@@ -0,0 +1,13 @@
+#include "DxLibPp.hpp"
+
+using namespace DxLibPp;
+
+int main() {
+    font fnt{};
+    while (system::update()) {
+		fnt.set_text("hogehoge");
+		fnt.draw();
+    }
+
+    return EXIT_SUCCESS;
+}
